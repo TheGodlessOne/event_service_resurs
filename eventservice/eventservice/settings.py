14a6419331a6fb2d77ae20bb38afb4e66b4e5434
TheGodlessOne <alexnikv@mail.ru>
2020-06-20 01:23:46 +0300

    cache added

--- eventservice/eventservice/settings.py
+++ eventservice/eventservice/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'eventservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'event_db',
+        'USER': 'admin_db',
+        'PASSWORD': 'eventmanager',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
