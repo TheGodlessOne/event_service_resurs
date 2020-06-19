e6f8fc7742d48d6c3c644b55cd183c32cad55755
TheGodlessOne <alexnikv@mail.ru>
2020-06-20 02:10:33 +0300

    added 'event' model in settings.py

--- eventservice/events/models.py
+++ eventservice/events/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model): 
+    event_id = models.AutoField(primary_key=True)
+    event_title = models.CharField(max_length=30)
+    event_description = models.TextField(max_length=280)
+    event_date = models.DateTimeField()
