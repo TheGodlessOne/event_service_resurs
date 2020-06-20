2fc94e2ffae9f11f04170c2838c0aee46543948b
TheGodlessOne <alexnikv@mail.ru>
2020-06-20 06:50:25 +0300

    user serializer added

--- eventservice/events/serializers.py
+++ eventservice/events/serializers.py
@@ -5,3 +5,10 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 'title', 'description', 'date']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    events = serializers.PrimaryKeyRelatedField(many=True, queryset=Event.objects.all())
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'events']
