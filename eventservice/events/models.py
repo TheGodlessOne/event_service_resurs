5689261db3debd4d25594edbb7a2b4dbcc673fe7
TheGodlessOne <alexnikv@mail.ru>
2020-06-20 06:50:56 +0300

    dependency with owner added

--- eventservice/events/models.py
+++ eventservice/events/models.py
@@ -6,3 +6,4 @@ class Event(models.Model):
     event_title = models.CharField(max_length=30)
     event_description = models.TextField(max_length=280)
     event_date = models.DateTimeField()
+    owner = models.ForeignKey('auth.User', related_name='events', on_delete=models.CASCADE)
