ef951ce0668f7d833dd69eed5323317b5c3799ba
TheGodlessOne <alexnikv@mail.ru>
2020-06-20 06:53:20 +0300

    import of UserSerializer added

--- eventservice/events/views.py
+++ eventservice/events/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from events.models import Event
-from events.serializers import EventSerializer
+from events.serializers import EventSerializer, UserSerializer
 from django.contrib.auth.models import User
 
 
