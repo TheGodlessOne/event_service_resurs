fbd98ec4d8bab41db17320d2a6f35ad452be18d3
TheGodlessOne <alexnikv@mail.ru>
2020-06-23 02:39:07 +0300

    added cors

--- eventservice/eventservice/settings.py
+++ eventservice/eventservice/settings.py
@@ -51,8 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'eventservice.urls'
 
